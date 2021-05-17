299def1036a8ca679ea8d0ea43f4e34f20d0b6f9
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-05-17 09:04:02 -0400

    Update main.go

--- AppEngine/prajwalkoirala.com/main.go
+++ AppEngine/prajwalkoirala.com/main.go
@@ -1,8 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+var err error
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("www")))
-	http.ListenAndServe(":8080", nil)
+	if folderExists("www") {
+		http.Handle("/", http.FileServer(http.Dir("www")))
+		err = http.ListenAndServe(":8080", nil)
+		handleError(err)
+	} else {
+		log.Println("Error: Failed to find www folder")
+	}
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func folderExists(foldername string) bool {
+	info, err := os.Stat(foldername)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
 }
