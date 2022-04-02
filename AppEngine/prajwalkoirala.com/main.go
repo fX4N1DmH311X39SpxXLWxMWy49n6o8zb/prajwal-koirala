9151c804cec613b46e112d2aedd774a2c938dddb
prajwal-koirala-0000 <102563715+prajwal-koirala-0000@users.noreply.github.com>
2022-04-02 07:02:05 -0400

    Update main.go

--- AppEngine/prajwalkoirala.com/main.go
+++ AppEngine/prajwalkoirala.com/main.go
@@ -5,15 +5,9 @@ import (
 	"net/http"
 )
 
-var (
-	err        error
-	filePath   = "www"
-	serverPort = ":8080"
-)
-
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(filePath)))
-	err = http.ListenAndServe(serverPort, nil)
+	http.Handle("/", http.FileServer(http.Dir("www")))
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Println(err)
 	}
